66277960a8530a4ad081bf8e3f86c49c718627b1
ShulongZheng <95286353+ShulongZheng@users.noreply.github.com>
2021-11-30 19:53:12 +0800

    The Arduino File of the breathe effect
    This code currently controls the breathing effect of a single Neopixel light ring, very smooth

---
+++ Breather_v1.1.ino
@@ -0,0 +1,114 @@
+#include <Adafruit_NeoPixel.h>
+
+#define PIN 12
+#define NUMPIXEL 30   //how many pixel on the strip.
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMPIXEL, PIN, NEO_GRB + NEO_KHZ800);
+
+int MinBrightness = 20;       //value 0-255
+int MaxBrightness = 100;      //value 0-255
+
+int numLoops1 = 10;
+int numLoops2 = 5;
+//int numLoops3 = 5;
+//int numLoops4 = 3;          //add new integer and value for more color's loop if needed.
+
+int fadeInWait = 30;          //lighting up speed, steps.
+int fadeOutWait = 50;         //dimming speed, steps.
+
+//---------------------------------------------------------------------------------------------------//
+
+void setup() {
+  strip.begin();
+  strip.show();
+}
+
+/*
+rgbBreathe(strip.Color(insert r,g,b color code),numLoops(refer to integer above), (duration for lights to hold before dimming. insert 0 to skip hold)
+rainbowBreathe(numLoops(refer to integer above),(duration for lights to hold before dimming. insert 0 to skip hold)
+*/
+
+void loop() {
+  
+  //---strip will stay lit for some time before dimming again.----
+  
+  rgbBreathe(strip.Color(255, 0, 0), numLoops1, 250); //red.
+  rainbowBreathe(numLoops2, 250);
+
+  //duplicate for more colors.
+}
+
+
+//Functions -----------------------------------------------------------------------------------------//
+
+
+void rgbBreathe(uint32_t c, uint8_t x, uint8_t y) {
+  for (int j = 0; j < x; j++) {
+    for (uint8_t b = MinBrightness; b < MaxBrightness; b++) {
+      strip.setBrightness(b * 255 / 255);
+      for (uint16_t i = 0; i < strip.numPixels(); i++) {
+        strip.setPixelColor(i, c);
+      }
+      strip.show();
+      delay(fadeInWait);
+    }
+    strip.setBrightness(MaxBrightness * 255 / 255);
+    for (uint16_t i = 0; i < strip.numPixels(); i++) {
+      strip.setPixelColor(i, c);
+      strip.show();
+      delay(y);
+    }
+    for (uint8_t b = MaxBrightness; b > MinBrightness; b--) {
+      strip.setBrightness(b * 255 / 255);
+      for (uint16_t i = 0; i < strip.numPixels(); i++) {
+        strip.setPixelColor(i, c);
+      }
+      strip.show();
+      delay(fadeOutWait);
+    }
+  }
+}
+
+void rainbowBreathe(uint8_t x, uint8_t y) {
+  for (int j = 0; j < x; j++) {
+    for (uint8_t b = MinBrightness; b < MaxBrightness; b++) {
+      strip.setBrightness(b * 255 / 255);
+      for (uint8_t i = 0; i < strip.numPixels(); i++) {
+        strip.setPixelColor(i, Wheel(i * 256 / strip.numPixels()));
+      }
+      strip.show();
+      delay(fadeInWait);
+    }
+    strip.setBrightness(MaxBrightness * 255 / 255);
+    for (uint8_t i = 0; i < strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel(i * 256 / strip.numPixels()));
+      strip.show();
+      delay(y);
+    }
+    for (uint8_t b = MaxBrightness; b > MinBrightness; b--) {
+      strip.setBrightness(b * 255 / 255);
+      for (uint8_t i = 0; i < strip.numPixels(); i++) {
+        strip.setPixelColor(i, Wheel(i * 256 / strip.numPixels()));
+      }
+      strip.show();
+      delay(fadeOutWait);
+    }
+  }
+}
+
+
+//NeoPixel Wheel for Rainbow---------------------------------------
+
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 140 - WheelPos;       //the value here means - for 255 the strip will starts with red, 127-red will be in the middle, 0 - strip ends with red.
+  if (WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if (WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
+
